4a7d8594fbbe72066317870288d1e2e42f376d89
Gustav <gustav.marcks@hotmail.com>
2021-11-22 22:57:56 +0100

    Kodoptimering i ActionResult Info.

--- Arbetsprov/Controllers/PriceController.cs
+++ Arbetsprov/Controllers/PriceController.cs
@@ -13,22 +13,17 @@ namespace Arbetsprov.Controllers
         private static readonly List<Price> pricelist = new List<Price>();
         private static readonly List<Price> sortedpricelist = new List<Price>();
         private static readonly List<Price> priceinfolist = new List<Price>();
-        
         //Koppling till databas
         static readonly SqlConnection sqlconn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DB;Integrated Security=True;");
-
         //SQL query från databasen
         static readonly SqlCommand sqlcmd = new SqlCommand("SELECT * FROM dbo.Price", sqlconn);
-
         //Metod som skickar värde till vy Index 
         public ActionResult Index()
         {
             //Öppnar connection till SQL
             sqlconn.Open();
-
             //Skapar en datareader
             SqlDataReader sqlrdr = sqlcmd.ExecuteReader();
-
             //Läser databas med hjälp av datareader
             while (sqlrdr.Read())
             {
@@ -50,42 +45,31 @@ namespace Arbetsprov.Controllers
             }
             //Stänger connection till sql efter att data är hämtat
             sqlconn.Close();
-
             //Returnerar data från listan pricelist till vy View
             return View(pricelist);
         }
         //Inmatning av SKU i adressfält
         [Route("{pricecode}")]
-
         //Metod som skickar värde till vy Info
         public ActionResult Info(string pricecode)
-        {
+        {   
             //Loopar igenom samtiga artiklar i prislistan
             foreach (var item in pricelist)
             {
                 //Jämför datum och CatalogEntryCode som matades in i adressfält
                 if (item.CatalogEntryCode == pricecode && DateTime.Now < item.ValidUntil && DateTime.Now > item.ValidFrom)
                 {
-                    //Adderar objekten till listan som gick igenom if-satsen
-                    sortedpricelist.Add(item);                 
-                }
-                //Fortsätter loop från början
-                else
-                {
-                    continue;
+                    sortedpricelist.Add(item);
                 }
             }
-            //Minsta pris för jämföra priser i föregående lista
-            decimal minprice = sortedpricelist.Min(p => p.UnitPrice);
-
-            foreach (var item in sortedpricelist)
+            //Loopar igenom sorterad lista och tar fram lägsta priser
+            foreach (Price item in sortedpricelist)
             {
-                if (item.UnitPrice == minprice)
+                if (item.UnitPrice == sortedpricelist.Min(p => p.UnitPrice))
                 {
                     priceinfolist.Add(item);
                 }
             }
-
             //Returnerar data från listan priceinfolist till vy Info
             return View(priceinfolist);
         }
